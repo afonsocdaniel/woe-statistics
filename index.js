e3066107d41dcf7ea770e504ee77696f6211859e
unknown <filipeaartiaga@gmail.com>
2022-11-15 16:43:15 -0300

    Added Professor and Refactoring

--- index.js
+++ index.js
@@ -2,25 +2,7 @@
     const { Client, GatewayIntentBits, ComponentAssertions } = require('discord.js');
     const phantom = require('phantom');
     const config = require('./config.json');
-    const prefix = "!";
-    const rankURL = "https://playxro.net/rankings/woe?page=";
-    const playerURL = "https://playxro.net/rankings/woe-profile/";
-
-    const CLASS = {
-        4008: "Lord Knight",
-        4009: "High Priest",
-        4010: "High Wizard",
-        4011: "Withesmith",
-        4012: "Sniper",
-        4013: "Assasin Cross",
-        4014: "Paladin",
-        4015: "Champion",
-        4016: "Professor",
-        4017: "Stalker",
-        4018: "Creator",
-        4019: "Clown",
-        4020: "Gypsy",        
-    }
+    const {JOB, SKILL, playerURL, rankURL, prefix} = require('./utils/constants');
 
     const client = new Client({ intents: [
         GatewayIntentBits.Guilds,
@@ -29,47 +11,24 @@
         GatewayIntentBits.MessageContent,
     ] });
     
-    // Register an event so that when the bot is ready, it will log a messsage to the terminal
+    
     client.on("messageCreate", (message) => {
         if(!message.content.startsWith(prefix) || message.author.bot) {
             return;
         }
 
         if(message.content.startsWith("!help")){
-            message.reply("To start the bot type !createcsv + number of pages of players on the ranking");
-        }else if(message.content.startsWith("!createcsv")){
+            message.reply("To start the bot type !rrank + the page you want to search");
+        }else if(message.content.startsWith("!rrank")){
             var pages = message.content.split(" ")[1];
-            message.reply("Starting the creation of CSV from "+ pages + " pages\n" + "https://docs.google.com/spreadsheets/d/1kEB5iktSLrQOyQ_2ZNkDIh71aRgGZ88HjbGi08Eycm0/edit?usp=sharing");
-            StartSearch(parseInt(pages));
-        }else if(message.content.startsWith("!test")){
-            var charId = message.content.split(" ")[1];
-            TestSkillCount(charId);
+            message.reply("Starting the creation of CSV on page "+ pages + " \n" + "https://docs.google.com/spreadsheets/d/1kEB5iktSLrQOyQ_2ZNkDIh71aRgGZ88HjbGi08Eycm0/edit?usp=sharing");
+            startCrawling(parseInt(pages));
         }
     });
 
-    async function TestSkillCount(charId){
-        const instance = await phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']);
-        const page = await instance.createPage();
-        const status = await page.open(playerURL+charId);
-    
-        if (status !== 'success') {
-        console.error(status);
-        await instance.exit();
-        return;
-        }
-    
-        const content = await page.property('content');
-        await instance.exit();
-    
-        //string manipulations here
-        //var basicInfo = content.split("{")[1];
-        var result = content.replace(/&quot;/g,'"').split("data-page=\"")[1].split("\"></div>")[0];
-        var skillCount = JSON.parse(result.split("skill_count\":")[1].split("\,\"kill_log")[0]);
-        console.log(skillCount);
-    }
-
-    async function StartSearch(pages){ 
-        var playerIDs = [];
+    async function startCrawling(pages){ 
+        var playerIDs = [];        
+        //to-do - uncomment this to search all pages        
         //for(var i = 1; i < pages+1; i++){  
             console.log("Scanning page:"+pages);     
             const instance = await phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']);
@@ -107,123 +66,96 @@
             const content = await page.property('content');
             await instance.exit();
         
-            //string manipulations here
-            //var basicInfo = content.split("{")[1];
             var result = content.replace(/&quot;/g,'"').split("data-page=\"")[1].split("\"></div>")[0];
             var basicInfo = JSON.parse(result.split("basic_info\":[")[1].split("]\,\"skill_count\"")[0]);
-            var skillCount = JSON.parse(result.split("skill_count\":")[1].split("\,\"kill_log")[0]);
+            var skillInfo = result.split("skill_count\":")[1].split("\,\"kill_log")[0];
         
-            var player = {
-                id: basicInfo.char_id,
-                name: basicInfo.name,
-                class: basicInfo.class,
-                max_hp: basicInfo.max_hp,
-                max_sp: basicInfo.max_sp,
-                guild: basicInfo.gName,
-                kill: basicInfo.kill_count,
-                death: basicInfo.death_count,
-                damage_done: basicInfo.damage_done,
-                damage_taken: basicInfo.damage_received,
-                hp_potion: basicInfo.hp_heal_potions,
-                sp_potion: basicInfo.sp_heal_potions,
-                yellow_gemstone: basicInfo.yellow_gemstones,
-                red_gemstone: basicInfo.yellow_gemstones,
-                blue_gemstone: basicInfo.blue_gemstones,
-                acid_demonstration: basicInfo.acid_demonstration,
-                support_skills: basicInfo.support_skills_used,
-                healing: basicInfo.healing_done,
-                wrong_support_skills: basicInfo.wrong_support_skills_used,
-                wrong_healing: basicInfo.wrong_healing_done,
-                sp_used: basicInfo.sp_used,
-                ammo_used: basicInfo.ammo_used,
-            }
-
-            addRow("Players", getPlayerRow(player));
-            if(player.class == "4010"){
-                HighWizardRow(player, skillCount);
-            }else if(player.class == "4012"){
-                SniperRow(player, skillCount);
-            }else if(player.class == "4018"){
-                CreatorRow(player, skillCount);
-            }else if(player.class == "4015"){
-                PaladinRow(player, skillCount);
-            }
+            addPlayer(basicInfo, skillInfo);
         }
-        console.log("Total Players: " +playerIDs.length);
+        console.log("Total Players Added: " +playerIDs.length);
     }
 
+    function addPlayer(basicInfo, skillInfo){
 
-    async function addRow(page, value){
-        const {googleSheets, auth, spreadsheetId} = await getAuthSheets();
+        var player = {
+            id: basicInfo.char_id,
+            name: basicInfo.name,
+            class: basicInfo.class,
+            max_hp: basicInfo.max_hp,
+            max_sp: basicInfo.max_sp,
+            guild: basicInfo.gName,
+            kill: basicInfo.kill_count,
+            death: basicInfo.death_count,
+            damage_done: basicInfo.damage_done,
+            damage_taken: basicInfo.damage_received,
+            hp_potion: basicInfo.hp_heal_potions,
+            sp_potion: basicInfo.sp_heal_potions,
+            yellow_gemstone: basicInfo.yellow_gemstones,
+            red_gemstone: basicInfo.yellow_gemstones,
+            blue_gemstone: basicInfo.blue_gemstones,
+            acid_demonstration: basicInfo.acid_demonstrations,
+            support_skills: basicInfo.support_skills_used,
+            healing: basicInfo.healing_done,
+            wrong_support_skills: basicInfo.wrong_support_skills_used,
+            wrong_healing: basicInfo.wrong_healing_done,
+            sp_used: basicInfo.sp_used,
+            ammo_used: basicInfo.ammo_used,
+        }
 
-        const row = await googleSheets.spreadsheets.values.append({
-            auth,
-            spreadsheetId,
-            range: page,
-            valueInputOption: 'RAW',
-            resource: value
-        })
+        addRow("Players", getPlayerRow(player));
+        switch(player.class){
+            case JOB.HIGH_WIZARD:
+                addHW(player, skillInfo);
+                break;
+            case JOB.SNIPER:
+                addSniper(player, skillInfo);
+                break;
+            case JOB.CREATOR:
+                addCreator(player, skillInfo);
+                break;
+            case JOB.PROFESSOR:
+                addProfessor(player, skillInfo);
+                break;
+            case JOB.PALADIN:
+                addPaladin(player, skillInfo);     
+                break;
+            //to-do add Gypsy and HP
+        }
+
     }
 
-    async function getAuthSheets(){
-        const auth = new google.auth.GoogleAuth({
-            keyFile: "credentials.json",
-            scopes: "https://www.googleapis.com/auth/spreadsheets"
-        })
+    function addHW(player, skillInfo){
+        var meteorStorm = getSkill(SKILL.METEOR_STORM, skillInfo);
 
-        const client = await auth.getClient();
-
-        const googleSheets = google.sheets({
-            version: "v4",
-            auth: client
-        })
-
-        const spreadsheetId = "1kEB5iktSLrQOyQ_2ZNkDIh71aRgGZ88HjbGi08Eycm0";
-
-        return{
-            auth,
-            client,
-            googleSheets,
-            spreadsheetId,
-        };
-    }
-
-    function HighWizardRow(player, skillCount){
-
-        var obj = JSON.stringify(skillCount);
-        var ms = 0;
-        
-        if(obj.includes("\id\":83")){
-            ms =  obj.split("\"id\":83,\"count\":")[1].split("}")[0];
-        }        
-
-        var playerRow = {
-            "values": [
-                [
-                    player.guild,
-                    player.name,
-                    player.damage_done,
-                    ms,
-                    player.kill,
-                    player.death,
-                    player.damage_done / ms,
-                    player.kill / ms,
-                    player.damage_done / player.kill,
-                    (player.damage_done / ms) / player.kill,
+        if(meteorStorm > 0){
+            //add HW DD
+            var playerRow = {
+                "values": [
+                    [
+                        player.guild,
+                        player.name,
+                        player.damage_done,
+                        meteorStorm,
+                        player.kill,
+                        player.death,
+                        player.damage_done / meteorStorm,
+                        player.kill / meteorStorm,
+                        player.damage_done / player.kill,
+                        (player.damage_done / meteorStorm) / player.kill,
+                    ]
                 ]
-            ]
+            }
+    
+            addRow("HW DD", playerRow);
+        }else{
+            //Add HW FS
+            //to-do
         }
-        addRow("High Wizard", playerRow);
+
     }
     
-    function SniperRow(player, skillCount){
-       
-        var obj = JSON.stringify(skillCount);
-        var ss = 0;
-        
-        if(obj.includes("\id\":382")){
-            ss =  obj.split("\"id\":382,\"count\":")[1].split("}")[0];
-        }        
+    function addSniper(player, skillInfo){
+        var sharpshoot = getSkill(SKILL.SHARPSHOOT, skillInfo);
 
         var playerRow = {
             "values": [
@@ -231,67 +163,90 @@
                     player.guild,
                     player.name,
                     player.damage_done,
-                    ss,
+                    sharpshoot,
                     player.kill,
                     player.death,
-                    player.damage_done / ss,
-                    player.kill / ss,
+                    player.damage_done / sharpshoot,
+                    player.kill / sharpshoot,
                     player.damage_done / player.kill,
-                    (player.damage_done / ss) / player.kill,
+                    (player.damage_done / sharpshoot) / player.kill,
                 ]
             ]
         }
+
         addRow("Sniper", playerRow);        
     }
 
-    function CreatorRow(player, skillCount){
-       
-        var obj = JSON.stringify(skillCount);
-        var ad = 0;
-        
-        if(obj.includes("\id\":490")){
-            ad =  obj.split("\"id\":490,\"count\":")[1].split("}")[0];
-        }        
+    function addProfessor(player, skillInfo){
+        var dispel = getSkill(SKILL.DISPEL, skillInfo);
+        var landProtector = getSkill(SKILL.LAND_PROTECTOR, skillInfo);
+        var mindBreaker = getSkill(SKILL.MIND_BREAKER, skillInfo);
+        var fiberLock = getSkill(SKILL.FIBER_LOCK, skillInfo);
 
-        var playerRow = {
-            "values": [
-                [
-                    player.guild,
-                    player.name,
-                    player.damage_done,
-                    ad,
-                    player.kill,
-                    player.death,
-                    player.damage_done / ad,
-                    player.kill / ad,
-                    player.damage_done / player.kill,
-                    (player.damage_done / ad) / player.kill,
+        console.log(dispel,landProtector,mindBreaker,fiberLock);
+
+        if( parseInt(dispel) >= parseInt(landProtector)){
+            console.log("adding prof fs " + dispel, landProtector);
+            //Add Prof FS
+            var playerRow = {
+                "values": [
+                    [
+                        player.guild,
+                        player.name,
+                        player.damage_taken,
+                        dispel,
+                        mindBreaker,
+                        fiberLock,
+                        player.damage_taken / player.death,
+                        player.death
+                    ]
                 ]
-            ]
-        }
-        addRow("Chem DD", playerRow);        
+            }
+            addRow("Prof FS", playerRow);    
+        } else{
+            //Add PROF DLP
+            //to-do
+            console.log("adding prof dlp "+ dispel, landProtector);
+        }   
     }
 
-    function PaladinRow(player, skillCount){
-       
-        var obj = JSON.stringify(skillCount);
-        var sac = 0, provoke = 0, endure =0, providence =0, pressure = 0;
-        
-        if(obj.includes("\id\":255")){
-            sac =  obj.split("\"id\":255,\"count\":")[1].split("}")[0];
-        }        
-        if(obj.includes("\id\":6")){
-            provoke =  obj.split("\"id\":6,\"count\":")[1].split("}")[0];
-        }              
-        if(obj.includes("\id\":8")){
-            endure =  obj.split("\"id\":8,\"count\":")[1].split("}")[0];
-        }               
-        if(obj.includes("\id\":256")){
-            providence =  obj.split("\"id\":256,\"count\":")[1].split("}")[0];
-        }      
-        if(obj.includes("\id\":367")){
-            pressure =  obj.split("\"id\":367,\"count\":")[1].split("}")[0];
-        }
+    function addCreator(player, skillInfo){
+    
+        var acidDemonstration = getSkill(SKILL.ACID_DEMONSTRATION, skillInfo);
+
+        if(acidDemonstration > 0){
+            //Add Chem DD
+            var playerRow = {
+                "values": [
+                    [
+                        player.guild,
+                        player.name,
+                        player.damage_done,
+                        acidDemonstration,
+                        player.kill,
+                        player.death,
+                        player.damage_done / acidDemonstration,
+                        player.kill / acidDemonstration,
+                        player.damage_done / player.kill,
+                        (player.damage_done / acidDemonstration) / player.kill,
+                    ]
+                ]
+            }
+            addRow("Chem DD", playerRow);    
+        } else{
+            //Add Chem SPP
+            //to-do
+        }   
+    }
+
+    function addPaladin(player, skillInfo){
+    
+        var sacrifice = getSkill(SKILL.SACRIFICE, skillInfo);
+        var provoke = getSkill(SKILL.PROVOKE, skillInfo);
+        var endure = getSkill(SKILL.ENDURE, skillInfo);
+        var providence = getSkill(SKILL.PROVIDENCE, skillInfo);
+        var pressure = getSkill(SKILL.PRESSURE, skillInfo);
+
 
         var playerRow = {
             "values": [
@@ -299,7 +254,7 @@
                     player.guild,
                     player.name,
                     player.damage_taken,
-                    sac,
+                    sacrifice,
                     provoke,
                     endure,
                     providence,
@@ -344,6 +299,50 @@
 
         return playerRow;
     }
+
+    function getSkill(skillId, skillInfo){
+        var skill = 0;
+        if(skillInfo.includes("\id\":"+skillId)){
+            skill =  skillInfo.split("\"id\":"+skillId+",\"count\":")[1].split("}")[0];
+        }
+        return skill;
+    }
     
+    async function getAuthSheets(){
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets"
+        })
+
+        const client = await auth.getClient();
+
+        const googleSheets = google.sheets({
+            version: "v4",
+            auth: client
+        })
+
+        const spreadsheetId = "1kEB5iktSLrQOyQ_2ZNkDIh71aRgGZ88HjbGi08Eycm0";
+
+        return{
+            auth,
+            client,
+            googleSheets,
+            spreadsheetId,
+        };
+    }
+
+    
+    async function addRow(page, value){
+        const {googleSheets, auth, spreadsheetId} = await getAuthSheets();
+
+        const row = await googleSheets.spreadsheets.values.append({
+            auth,
+            spreadsheetId,
+            range: page,
+            valueInputOption: 'RAW',
+            resource: value
+        })
+    }
+
     // client.login logs the bot in and sets it up for use. You'll enter your token here.
     client.login(config.token);
---
+++ utils/constants.js
@@ -0,0 +1,43 @@
+const JOB = {
+    HIGH_WIZARD: 4010,
+    SNIPER: 4012,
+    PALADIN: 4015,
+    PROFESSOR: 4017,
+    CREATOR:  4018,     
+}
+
+const SKILL = {
+    //HIGH WIZARD
+    METEOR_STORM: 83,
+    //SNIPER
+    SHARPSHOOT: 382,
+    //CREATOR
+    ACID_DEMONSTRATION: 490,
+    SLIM_POTION_PITCHER: 0,
+    //PALADIN
+    SACRIFICE: 255,
+    PROVOKE: 6,
+    ENDURE: 8,
+    PROVIDENCE: 256,
+    PRESSURE: 367,
+    //PROFESSOR
+    DISPEL: 289,
+    MIND_BREAKER: 402,
+    FIBER_LOCK: 405,
+    LAND_PROTECTOR: 288,
+    //HP
+    BLESSING: 0,
+    AGI: 0,
+    RECOVERY: 0,
+    LEX_AETHERNA: 0,
+}
+
+const rankURL = "https://playxro.net/rankings/woe?page=";
+const playerURL = "https://playxro.net/rankings/woe-profile/";
+const prefix = "!";
+
+exports.JOB = JOB;
+exports.SKILL = SKILL;
+exports.rankURL = rankURL;
+exports.playerURL = playerURL;
+exports.prefix = prefix;
